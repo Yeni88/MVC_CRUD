fc52a1fa2e93c65f8e740279fc152a58fe1c008f
Yenifer Alfaro <yalfaro@grupomas.com>
2021-02-16 16:22:15 -0600

    CRUD

---
+++ CRUD/CRUD.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRUD
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CRUD" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CRUD.svc or CRUD.svc.cs at the Solution Explorer and start debugging.
+    public class CRUD : ICRUD
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
