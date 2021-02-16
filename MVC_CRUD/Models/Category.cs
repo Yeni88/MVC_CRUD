33c0ad97acec5f982310df730ef188ffbc6ba235
Yenifer Alfaro <yalfaro@grupomas.com>
2021-02-16 16:31:20 -0600

    Model

---
+++ MVC_CRUD/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfRest.Model
+{
+    public class Category
+    {
+        //public tipo nombre_atrib { get; set; }
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set }
+        public string Description { get; set; }
+    }
+}
---
+++ MVC_CRUD/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfRest.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public Supplier supplier { get; set; }
+
+        public Category category { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int UnitsInStock { get; set; }
+
+        public int UnitsOnOrder { get; set; }
+
+        public int ReorderLevel { get; set; }
+
+        public int Discontinued { get; set; }
+
+
+    }
+}
