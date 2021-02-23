eb9b23d49a4fb22eb3b050c05292ad681dd4f505
Yenifer Alfaro <yalfaro@grupomas.com>
2021-02-23 17:24:22 -0600

    delete

--- CRUD/CRUD.svc.cs
+++ CRUD/CRUD.svc.cs
@@ -10,170 +10,174 @@ using MVC_CRUD.Entities.Models;
 
 namespace CRUD
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CRUD" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select CRUD.svc or CRUD.svc.cs at the Solution Explorer and start debugging.
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CRUD" in code, svc and config file together.
+// NOTE: In order to launch WCF Test Client for testing this service, please select CRUD.svc or CRUD.svc.cs at the Solution Explorer and start debugging.
     
-    public class CRUD : ICRUD
+public class CRUD : ICRUD
+{
+
+    /// <summary>
+    /// GET PRODUCTS
+    /// </summary>
+    /// <returns>
+    /// List ProductEntity
+    /// </returns>
+    public List<ProductEntity> GetProducts()
     {
-
-        /// <summary>
-        /// GET PRODUCTS
-        /// </summary>
-        /// <returns>
-        /// List ProductEntity
-        /// </returns>
-        public List<ProductEntity> GetProducts()
+        try
         {
-            try
+            ConnectionDB ConnectionManagerInstance = new ConnectionDB();
+            using (IDbConnection connection = ConnectionManagerInstance.GetConnection("ConnDB"))
             {
-                ConnectionDB ConnectionManagerInstance = new ConnectionDB();
-                using (IDbConnection connection = ConnectionManagerInstance.GetConnection("ConnDB"))
-                {
-                    var resultadoGP = connection.Query<ProductEntity, SupplierEntity, CategoryEntity, ProductEntity>(
-                         "sp_ShowAllProducts",
-                         (p, s, c) =>
-                         {
-                             p.Supplier = s;
-                             p.Category = c;
-                             return p;
-                         },
-                         splitOn: "SupplierID,CategoryID",
-                         commandType: CommandType.StoredProcedure).ToList<ProductEntity>();
+                var resultadoGP = connection.Query<ProductEntity, SupplierEntity, CategoryEntity, ProductEntity>(
+                        "sp_ShowAllProducts",
+                        (p, s, c) =>
+                        {
+                            p.Supplier = s;
+                            p.Category = c;
+                            return p;
+                        },
+                        splitOn: "SupplierID,CategoryID",
+                        commandType: CommandType.StoredProcedure).ToList<ProductEntity>();
 
-                    return resultadoGP;
+                return resultadoGP;
 
-                }
             }
-            catch (Exception ex)
-            {
+        }
+        catch (Exception ex)
+        {
 
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
+        }
           
-        }
+    }
        
-        /// <summary>
-        /// ADD PRODUCTS
-        /// </summary>
-        /// <param name="product"></param>
-        /// <returns>
-        /// Product
-        /// </returns>
-        public ProductEntity AddProducts(ProductEntity product){
-            try
+    /// <summary>
+    /// ADD PRODUCTS
+    /// </summary>
+    /// <param name="product"></param>
+    /// <returns>
+    /// Product
+    /// </returns>
+    public ProductEntity AddProducts(ProductEntity product){
+        try
+        {
+            ConnectionDB ConnectionManagerInstance = new ConnectionDB();
+            using (IDbConnection connection = ConnectionManagerInstance.GetConnection("ConnDB"))
             {
-                ConnectionDB ConnectionManagerInstance = new ConnectionDB();
-                using (IDbConnection connection = ConnectionManagerInstance.GetConnection("ConnDB"))
-                {
-                    var resultadAP = connection.QuerySingleOrDefault<ProductEntity>("sp_addNewProducts",
+                var resultadAP = connection.QuerySingleOrDefault<ProductEntity>("sp_addNewProducts",
 
-                        param: new
-                        {
-                            product.ProductName,
-                            product.SupplierID,
-                            product.CategoryID,
-                            product.QuantityPerUnit,
-                            product.UnitPrice,
-                            product.UnitsInStock,
-                            product.UnitsOnOrder,
-                            product.ReorderLevel,
-                            product.Discontinued
-                        },
-                          commandType: CommandType.StoredProcedure);
+                    param: new
+                    {
+                        product.ProductName,
+                        product.SupplierID,
+                        product.CategoryID,
+                        product.QuantityPerUnit,
+                        product.UnitPrice,
+                        product.UnitsInStock,
+                        product.UnitsOnOrder,
+                        product.ReorderLevel,
+                        product.Discontinued
+                    },
+                        commandType: CommandType.StoredProcedure);
 
-                    return resultadAP;
-                }
+                return resultadAP;
             }
-            catch (Exception ex)
-            {
+        }
+        catch (Exception ex)
+        {
 
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
+        }
             
-        }
+    }
 
-        /// <summary>
-        /// GET CATEGORIES
-        /// </summary>
-        /// <returns>
-        /// List CategoryEntity
-        /// </returns>
-        public List<CategoryEntity> GetCategories()
+    /// <summary>
+    /// GET CATEGORIES
+    /// </summary>
+    /// <returns>
+    /// List CategoryEntity
+    /// </returns>
+    public List<CategoryEntity> GetCategories()
+    {
+        try
         {
-            try
-            {
-                ConnectionDB connection = new ConnectionDB();
-                using (IDbConnection conn = connection.GetConnection("ConnDB")) {
+            ConnectionDB connection = new ConnectionDB();
+            using (IDbConnection conn = connection.GetConnection("ConnDB")) {
 
-                    var resultadoGP = conn.Query<CategoryEntity>(
-                         "sp_ShowCategories",
-                         commandType: CommandType.StoredProcedure).ToList<CategoryEntity>();
+                var resultadoGP = conn.Query<CategoryEntity>(
+                        "sp_ShowCategories",
+                        commandType: CommandType.StoredProcedure).ToList<CategoryEntity>();
 
-                    return resultadoGP;
+                return resultadoGP;
                 
-                }
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception (ex.Message);
             }
         }
-
-       /// <summary>
-       /// GET SUPPLIER
-       /// </summary>
-       /// <returns>
-       /// List SupplierEntity
-       /// </returns>
-        public List<SupplierEntity> GetSuppliers()
+        catch (Exception ex)
         {
-            try
-            {
-                ConnectionDB connection = new ConnectionDB();
-                using (IDbConnection conn = connection.GetConnection("ConnDB"))
-                {
 
-                    var resultadoGS = conn.Query<SupplierEntity>(
-                         "sp_ShowSuppliers",
-                         commandType: CommandType.StoredProcedure).ToList<SupplierEntity>();
-
-                    return resultadoGS;
-
-                }
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public ProductEntity DeleteProducts(int? ID)
-        {
-            try
-            {
-
-                ConnectionDB ConnectionManagerInstance = new ConnectionDB();
-                using (IDbConnection connection = ConnectionManagerInstance.GetConnection("ConnDB"))
-                {
-                    var resultadAP = connection.QuerySingleOrDefault<ProductEntity>("sp_DiscontinuedProduct",
-
-                        param: new
-                        {
-                            @ProductID_s = ID,
-                            Disc = 0
-                        },
-                          commandType: CommandType.StoredProcedure);
-
-                    return resultadAP;
-                    }
-                }
-            catch (Exception ex)
-            {
-
-                throw new Exception (ex.Message);
-            }
+            throw new Exception (ex.Message);
         }
     }
+
+    /// <summary>
+    /// GET SUPPLIER
+    /// </summary>
+    /// <returns>
+    /// List SupplierEntity
+    /// </returns>
+    public List<SupplierEntity> GetSuppliers()
+    {
+        try
+        {
+            ConnectionDB connection = new ConnectionDB();
+            using (IDbConnection conn = connection.GetConnection("ConnDB"))
+            {
+
+                var resultadoGS = conn.Query<SupplierEntity>(
+                        "sp_ShowSuppliers",
+                        commandType: CommandType.StoredProcedure).ToList<SupplierEntity>();
+
+                return resultadoGS;
+
+            }
+        }
+        catch (Exception ex)
+        {
+
+            throw new Exception(ex.Message);
+        }
+    }
+    /// <summary>
+    /// DELETE PRODUCTS
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <returns>Product Identity</returns>
+    public ProductEntity DeleteProducts(int? ID)
+    {
+        try
+        {
+
+            ConnectionDB ConnectionManagerInstance = new ConnectionDB();
+            using (IDbConnection connection = ConnectionManagerInstance.GetConnection("ConnDB"))
+            {
+                var resultadAP = connection.QuerySingleOrDefault<ProductEntity>("sp_DiscontinuedProduct",
+
+                    param: new
+                    {
+                        @ProductID_s = ID,
+                        Disc = 0
+                    },
+                        commandType: CommandType.StoredProcedure);
+
+                return resultadAP;
+                }
+            }
+        catch (Exception ex)
+        {
+
+            throw new Exception (ex.Message);
+        }
+    }
+}
 }
